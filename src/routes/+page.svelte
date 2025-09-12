4120ba75e592e7656f64b34232ae1d9afd83d61b
Aditya <adityakb2005@gmail.com>
2025-09-12 11:48:36 +0530

    feat: Add initial layout structure and sidebar component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+</script>
+
+<svelte:head>
+	<title></title>
+</svelte:head>
+
+<main>
+    <div class="bg-zinc-50 dark:bg-zinc-900">
+        <aside>
+
+        </aside>
+    </div>
+</main>
