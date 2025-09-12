16149c241f777adef9d86412b42de9ad8701a928
Aditya <adityakb2005@gmail.com>
2025-09-12 12:44:37 +0530

    feat: Add Buttons component and enhance Sidebar with additional icons

---
+++ src/lib/components/Buttons.svelte
@@ -0,0 +1,13 @@
+<script>
+	let { name, icon = null } = $props();
+	const StartingIcon = icon;
+</script>
+
+<button
+	class="flex w-full items-center bg-inherit px-5 py-2 text-base text-zinc-900 hover:bg-zinc-300 dark:text-zinc-100 dark:hover:bg-zinc-700 overflow-hidden"
+>
+	{#if StartingIcon}
+		<StartingIcon class="mr-2 inline-block h-5 w-5" />
+	{/if}
+	<span class="truncate whitespace-nowrap">{name}</span>
+</button>
--- src/lib/components/Sidebar.svelte
+++ src/lib/components/Sidebar.svelte
@@ -1,6 +1,14 @@
 <script>
+    import Buttons from "./Buttons.svelte";
+     import { Bot, Calendar, House, NotepadText, PartyPopper, Pen, Settings } from '@lucide/svelte';
 </script>
 
-<div class="bg-zinc-200 dark:bg-zinc-900 min-h-screen flex w-60">
-
+<div class="bg-zinc-200 dark:bg-zinc-900 min-h-screen flex flex-col pt-5 w-60 gap-2">
+    <Buttons name="Home" icon={House}/>
+    <Buttons name="Queries AI" icon={Bot}/>
+    <Buttons name="Todo" icon={NotepadText}/>
+    <Buttons name="Schedule" icon={Calendar}/>
+    <Buttons name="Events" icon={PartyPopper}/>
+    <Buttons name="Appointment" icon={Pen}/>
+    <Buttons name="Settings" icon={Settings}/>
 </div>
